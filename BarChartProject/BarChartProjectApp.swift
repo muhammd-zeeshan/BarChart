d2ecb7d8e4fb23bff0ce8d4e4d1720015a906e5f
muhmmad-zeeshan <rafaq657@gmail.com>
2024-09-14 21:34:32 +0500

    Initial Commit

---
+++ BarChartProject/BarChartProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartProjectApp.swift
+//  BarChartProject
+//
+//  Created by Muhammad Zeeshan on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarChartProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
